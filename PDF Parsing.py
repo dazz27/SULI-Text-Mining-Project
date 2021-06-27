123a9291084871ed9f672bc1fa1d5d27d1a52fc0
Darrien Hunt <56241512+dazz27@users.noreply.github.com>
2021-06-27 19:15:20 -0400

    Add files via upload

--- PDF Parsing.py
+++ PDF Parsing.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 from urllib.parse import urljoin
 
-import fitz
+#import fitz
 
 def extract_information(pdf_path):
     with open(pdf_path, 'rb') as f:
@@ -38,31 +38,51 @@ def extract_information(pdf_path):
 def search_bioRxiv(queries):
     for query in queries:
         query = query.replace(" ", "%252B")
-        URL = "https://www.biorxiv.org/search/" + query + "%20jcode%3Abiorxiv%20numresults%3A10%20sort%3Arelevance-rank%20format_result%3Astandard"
+        URL = "https://www.biorxiv.org/search/" + query + "%20jcode%3Abiorxiv%20numresults%3A75%20sort%3Arelevance-rank%20format_result%3Astandard"
         req = urllib.request.Request(URL, headers={'User-Agent' : "Magic Browser"})
         response = urllib.request.urlopen( req )
         html = response.read()
+
         # Parsing response
         soup = BeautifulSoup(html, 'html.parser')
+
         # Extracting number of results
         resultStats = soup.find(id="page-title").string
         print(resultStats)
+
+        #Retrieving page numbers
+        page_list = soup.find('div', class_='highwire-list page-group-items item-list')
+        pages = page_list.find_all('a')
+        page_num = int(pages[-1].get_text())
+        print(page_num)
+
         #Accessing articles
-        results = soup.find_all(href=True, class_='highwire-cite-linked-title', limit=3)
-        for link in results:
-            new_URL = "https://www.biorxiv.org" + link['href'] + ".full.pdf"
-            #Name the pdf files using the last portion of each link which are unique in this case
-            filename = os.path.join(floc ,link['href'].split('/')[-1])
-            with open(filename, 'wb') as f:
-                f.write(requests.get(new_URL).content)
+        for x in range(0, page_num):
+            y = str(x)
+            URL = "https://www.biorxiv.org/search/" + query + "%20jcode%3Abiorxiv%20numresults%3A75%20sort%3Arelevance-rank%20format_result%3Astandard?page=" + y
+            req = urllib.request.Request(URL, headers={'User-Agent' : "Magic Browser"})
+            response = urllib.request.urlopen( req )
+            html = response.read()
+
+            # Parsing response
+            soup = BeautifulSoup(html, 'html.parser')
+
+            results = soup.find_all(href=True, class_='highwire-cite-linked-title')
+            for link in results:
+                new_URL = "https://www.biorxiv.org" + link['href'] + ".full.pdf"
+                #Name the pdf files using the last portion of each link which are unique in this case
+                filename = os.path.join(floc ,link['href'].split('/')[-1])
+                with open(filename, 'wb') as f:
+                    f.write(requests.get(new_URL).content)
 
 if __name__ == '__main__':
    
     #File location that PDFs will be downloaded to in the virtual environment
-    floc = "/home/dhunt/py-venv/2021-SULI-Text-Mining1/SULI-Text-Mining-Project"
+    #floc = "/home/dhunt/py-venv/2021-SULI-Text-Mining1/SULI-Text-Mining-Project"
+    floc = "/hpcgpfs01/scratch/dhuntSULI2021/2021-SULI-Text-Mining/SULI-Text-Mining-Project/bioRxiv_Articles"
 
     #search_queries = ["P-loop NTPase", "P-loop GTPase", "Fe protein", "Walker A motif", "nitrogenase reductase", "Nif-like", "dinitrogenase", "MoFe protein", "heterotetramer", "Molybdenum nitrogenase"]
-    search_queries = ["P-loop NTPase"]
+    search_queries = ["protein"]
 
     search_bioRxiv(search_queries)
 
